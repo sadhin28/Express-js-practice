53c3d460eb42b6e781ece6d1312df0dec1e51f24
sadhin28 <taosifsadhin113@gmail.com>
2025-06-16 21:27:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express();
+const port = 5000;
+
+app.get('/',(req,res)=>{
+        res.send('Hellow world')
+})
+app.get('/data',(req,res)=>{
+    res.send('Taosif Bin Sadhin')
+})
+app.listen(port,()=>{
+    console.log(`My first server is running on post : ${port}`)
+})
